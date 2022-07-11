f99a53e8084f1f070d82ed81983f5e95af02fdd9
Edward Wynman <edward@ISCINDUSTRIES.LOCAL>
2022-07-11 15:30:36 -0400

    Updating Resume, Formatting code

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var navbar = document.getElementById("navbarNav");
-        function hideShow() {
-          console.log("click")
-          if (navbar.style.display == "block") {
-            navbar.style.display = "none";
-            console.log("disapper")
-          } else {
-            navbar.style.display = "block";
-            console.log("visible")
-          }
-        }
+function hideShow() {
+  console.log("click");
+  if (navbar.style.display == "block") {
+    navbar.style.display = "none";
+    console.log("disapper");
+  } else {
+    navbar.style.display = "block";
+    console.log("visible");
+  }
+}
