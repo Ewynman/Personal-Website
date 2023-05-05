a446b91c9a72007477272d7e39f923ed79a6d2cd
Eddie Wynman <edward.wynman@gmail.com>
2023-05-04 21:37:18 -0400

    Merge Redesign-May-2023 branch

--- scripts.js
+++ scripts.js
@@ -34,48 +34,50 @@
   });
 })(jQuery); // End of use strict
 
+// TImeline JS
+$(function () {
+  window.sr = ScrollReveal();
 
+  if ($(window).width() < 768) {
+    if ($(".timeline-content").hasClass("js--fadeInLeft")) {
+      $(".timeline-content")
+        .removeClass("js--fadeInLeft")
+        .addClass("js--fadeInRight");
+    }
 
-// Light and dark mode
-
-// Get the button element
-const modeToggleBtn = document.getElementById('mode-toggle-btn');
-
-// Get the stylesheet elements for both light and dark mode
-const lightModeStylesheet = document.querySelector('link[href="style.css"]');
-const darkModeStylesheet = document.createElement('link');
-darkModeStylesheet.setAttribute('rel', 'stylesheet');
-darkModeStylesheet.setAttribute('href', 'darkmode.css');
-
-// Check if the user has a preference for dark mode
-const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-// Set the initial mode based on the user's preference or default to light mode
-let isDarkMode = userPrefersDark || false;
-setMode(isDarkMode);
-
-// Add an event listener to the button
-modeToggleBtn.addEventListener('click', function() {
-  isDarkMode = !isDarkMode;
-  setMode(isDarkMode);
-});
-
-// Function to set the mode based on the isDarkMode variable
-function setMode(isDarkMode) {
-  if (isDarkMode) {
-    // Set the stylesheet to dark mode
-    document.head.appendChild(darkModeStylesheet);
-    // Remove the stylesheet for light mode
-    lightModeStylesheet.remove();
-    // Set a data attribute on the body element to indicate the current mode
-    document.body.setAttribute('data-theme', 'dark');
+    sr.reveal(".js--fadeInRight", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
   } else {
-    // Set the stylesheet to light mode
-    document.head.appendChild(lightModeStylesheet);
-    // Remove the stylesheet for dark mode
-    darkModeStylesheet.remove();
-    // Remove the data attribute on the body element to indicate the current mode
-    document.body.removeAttribute('data-theme');
-  }
-}
+    sr.reveal(".js--fadeInLeft", {
+      origin: "left",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
 
+    sr.reveal(".js--fadeInRight", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800,
+    });
+  }
+
+  sr.reveal(".js--fadeInLeft", {
+    origin: "left",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800,
+  });
+
+  sr.reveal(".js--fadeInRight", {
+    origin: "right",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800,
+  });
+});
