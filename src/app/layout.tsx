b91912774a9c330e96e617dd1bd8da6c8e7a07ac
Edward Wynman <92377881+Ewynman@users.noreply.github.com>
2025-08-12 11:30:32 -0400

    Update jobs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.jpeg" type="image/jpeg" />
+      </head>
       <body suppressHydrationWarning>
         <Header />
         <main>{children}</main>
